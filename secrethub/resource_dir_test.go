886db2f13d880198a821a8623b584301aceb49c6
Simon Barendse <simon.barendse@gmail.com>
2021-02-04 14:23:31 +0100

    Add acceptance tests for dir datasource and resource

---
+++ secrethub/resource_dir_test.go
@@ -0,0 +1,47 @@
+package secrethub
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccResourceDir(t *testing.T) {
+	config := fmt.Sprintf(`
+		resource "secrethub_dir" "%v" {
+			path = "%v"
+		}
+	`, testAcc.dirName, testAcc.dirPath)
+
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  testAccPreCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					checkDirExistsRemotely(testAcc.dirPath),
+				),
+			},
+		},
+	})
+}
+
+func checkDirExistsRemotely(path string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := *testAccProvider.Meta().(providerMeta).client
+
+		exists, err := client.Dirs().Exists(path)
+		if err != nil {
+			return err
+		}
+
+		if !exists {
+			return fmt.Errorf("expected directory to exist: %s", path)
+		}
+
+		return nil
+	}
+}
